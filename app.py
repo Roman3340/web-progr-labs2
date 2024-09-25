bac671c7c3c5325e121fbf96d80a499f5188e671
Чукаев Роман <roman.chukaev1@gmail.com>
2024-09-25 23:29:27 +0700

    Имя студента

--- app.py
+++ app.py
@@ -201,4 +201,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Роман Чукаев'
+    return render_template('example.html', name=name)
