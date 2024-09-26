634a2e74b474841f1f6bf045f8c508409c4b2779
Чукаев Роман <roman.chukaev1@gmail.com>
2024-09-26 09:03:49 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -205,4 +205,13 @@ def example():
     numberOfLab = '2'
     groupStudent = 'ФБИ-24'
     numberOfCourse = '3 курс'
-    return render_template('example.html')
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', name=name, numberOfLab=numberOfLab,
+                           groupStudent=groupStudent, numberOfCourse=numberOfCourse, 
+                           fruits=fruits)
