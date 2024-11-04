540a65d4d5621df69cfd458bf127ac8e0a1fd9bd
Чукаев Роман <roman.chukaev1@gmail.com>
2024-11-04 14:06:39 +0700

    проверка бд7

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, current_app
 import psycopg2
 lab5 = Blueprint('lab5', __name__)
 
@@ -17,23 +17,32 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'roman_chukaev_knowledge_base',
-        user = 'roman_chukaev_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    try:
+        # Попытка соединения с базой данных
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='roman_chukaev_knowledge_base',
+            user='roman_chukaev_knowledge_base',
+            password='123'
+        )
+        cur = conn.cursor()
 
-    cur.execute(f"SELECT login_user FROM users WHERE login_user='{login}';")
-    if cur.fetchone():
+        # Проверка на существование пользователя
+        cur.execute("SELECT login_user FROM users WHERE login_user = %s;", (login,))
+        if cur.fetchone():
+            cur.close()
+            conn.close()
+            return render_template('lab5/register.html', error='Такой пользователь уже существует')
+
+        # Вставка нового пользователя
+        cur.execute("INSERT INTO users (login_user, password_user) VALUES (%s, %s);", (login, password))
+        conn.commit()
         cur.close()
         conn.close()
-        return render_template('lab5/register.html', error='Такой пользователь уже существует')
-    
-    cur.execute(f"INSERT INTO users (login_user, password_user) VALUES ('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
-    return render_template('lab5/success.html', login=login, error=None)
+        return render_template('lab5/success.html', login=login, error=None)
+
+    except psycopg2.Error as db_error:
+        # Логирование ошибки базы данных
+        current_app.logger.error(f"Ошибка базы данных: {db_error}")
+        return render_template('lab5/register.html', error="Ошибка подключения к базе данных")
     
