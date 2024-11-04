3bd319acb0161a29b9a2b3e70e76a313c3880929
Чукаев Роман <roman.chukaev1@gmail.com>
2024-11-04 13:28:48 +0700

    проверка бд3

--- app.py
+++ app.py
@@ -91,25 +91,25 @@ def not_found_404(err):
 '''
 
 
-@app.errorhandler(500)
-def not_found_500(err):
-    return '''
-<!doctype html>
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-        <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
+# @app.errorhandler(500)
+# def not_found_500(err):
+#     return '''
+# <!doctype html>
+# <html>
+#     <head>
+#         <title>НГТУ, ФБ, Лабораторные работы</title>
+#         <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+#     </head>
+#     <body>
+#         <header>
+#             НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+#         </header>
 
-        <h2>Ошибка 500 - сервер не смог обработать запрос</h2>
+#         <h2>Ошибка 500 - сервер не смог обработать запрос</h2>
 
-        <footer>
-            &copy; Чукаев Роман, ФБИ-24, 3 курс, 2024
-        </footer>
-    </body>
-</html>
-'''
+#         <footer>
+#             &copy; Чукаев Роман, ФБИ-24, 3 курс, 2024
+#         </footer>
+#     </body>
+# </html>
+# '''
