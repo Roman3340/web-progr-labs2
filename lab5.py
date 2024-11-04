8d90dbbc887469dfaf5d5c39bafd9edd39ba1719
Чукаев Роман <roman.chukaev1@gmail.com>
2024-11-04 18:07:06 +0700

    исправление SQL-инъекции

--- lab5.py
+++ lab5.py
@@ -39,14 +39,14 @@ def register():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login_user FROM users WHERE login_user='{login}';")
+    cur.execute("SELECT login_user FROM users WHERE login_user=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error='Такой пользователь уже существует')
     
     password_hash = generate_password_hash(password)
 
-    cur.execute(f"INSERT INTO users (login_user, password_user) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login_user, password_user) VALUES (%s, %s);", (login, password_hash))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login, error=None)
     
@@ -63,7 +63,7 @@ def login():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login_user='{login}';")
+    cur.execute("SELECT * FROM users WHERE login_user=%s;", (login, ))
     user = cur.fetchone()
     if not user:
         db_close(conn, cur)
@@ -96,7 +96,7 @@ def create():
     cur.execute("SELECT * FROM users WHERE login_user=%s;", (login, ))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) VALUES ({login_id}, '{title}', '{article_text}')")
+    cur.execute("INSERT INTO articles(user_id, title, article_text) VALUES (%s, %s, %s);", (login_id, title, article_text))
     # cur.fetchone()
 
     db_close(conn, cur)
@@ -111,10 +111,10 @@ def list():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT id FROM users WHERE login_user='{login}';")
+    cur.execute("SELECT id FROM users WHERE login_user=%s;", (login, ))
     login_id = cur.fetchone()["id"]
 
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{login_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (login_id, ))
     articles = cur.fetchall()
 
     db_close(conn, cur)
