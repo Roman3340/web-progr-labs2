3a798cfe6056c797926d2e36c3b5c64625e864fb
Чукаев Роман <roman.chukaev1@gmail.com>
2024-11-04 12:14:38 +0700

    ошибка ввода л и п 1

--- lab5.py
+++ lab5.py
@@ -4,3 +4,14 @@ lab5 = Blueprint('lab5', __name__)
 @lab5.route('/lab5/')
 def lab():
     return render_template('lab5/lab5.html')
+
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/register.html', error='Заполните все поля')
