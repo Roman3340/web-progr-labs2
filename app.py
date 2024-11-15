dcd4b2c03a0cef15d5e09a57883f30893019147a
Чукаев Роман <roman.chukaev1@gmail.com>
2024-11-15 22:01:29 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
@@ -15,6 +16,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 app.secret_key = 'секретно-секретный секрет'
@@ -46,6 +48,7 @@ def menu():
             <li><a href="/lab3/">Третья лабораторная</a></li>
             <li><a href="/lab4/">Четвертая лабораторная</a></li>
             <li><a href="/lab5/">Пятая лабораторная</a></li>
+            <li><a href="/lab6/">Шестая лабораторная</a></li>
         </ul>
 
         <footer>
@@ -71,6 +74,14 @@ def lab_3():
 @app.route('/lab4/')
 def lab_4():
     return render_template('lab4/lab4.html')
+
+@app.route('/lab5/')
+def lab_5():
+    return render_template('lab5/lab5.html')
+
+@app.route('/lab6/')
+def lab_6():
+    return render_template('lab6/lab6.html')
 
 
 @app.errorhandler(404)
